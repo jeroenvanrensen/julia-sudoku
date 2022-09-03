1df4b26437caae61bfd28aa073c454bdcc6c901c
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2022-09-03 10:13:51 +0200

    Reorganize test suite

--- test.jl
+++ test.jl
@@ -2,22 +2,22 @@ include("main.jl")
 using Test, Random
 using .SudokuSolver: fillMissingDigit
 
-println("⌛ Running tests...")
+@testset verbose = true "Sudoku Solver" begin
+    @testset "It can fill one missing digit" begin
+        for i in 1:1000
+            # It can fill when one digit is missing
+            correct = shuffle(1:9)
+            incorrect = replace(correct, rand(1:9) => 0)
+            @test fillMissingDigit(incorrect) == correct
 
-for i in 1:1000
-    # it fills when one digit is empty
-    correct = shuffle(1:9)
-    incorrect = replace(correct, rand(1:9) => 0)
-    @test fillMissingDigit(incorrect) == correct
+            # It stays the same when all digits are filled in
+            correct = shuffle(1:9)
+            @test fillMissingDigit(correct) == correct
 
-    # it keeps the same when all digits are filled
-    correct = shuffle(1:9)
-    @test fillMissingDigit(correct) == correct
-
-    # it does not fill in when two or more digits are missing
-    correct = shuffle(1:9)
-    incorrect = replace(replace(correct, rand(1:4) => 0), rand(5:9) => 0)
-    @test fillMissingDigit(incorrect) == incorrect
+            # It stays the same when two or more digits are missing
+            correct = shuffle(1:9)
+            incorrect = replace(replace(correct, rand(1:4) => 0), rand(5:9) => 0)
+            @test fillMissingDigit(incorrect) == incorrect
+        end
+    end
 end
-
-println("✅ Tests passed")
